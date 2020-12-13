d672f92b994df3a0eb4ae97a260b2a03aef3d500
Quan Dang <18520339@gm.uit.edu.vn>
2020-12-13 23:45:08 +0700

    Update Users.sql

--- DATABASE/Users.sql
+++ DATABASE/Users.sql
@@ -1,11 +1,11 @@
-﻿USE QuanLyHocSinh
+USE QuanLyHocSinh
 GO
 
 CREATE TRIGGER ThemNguoiDung ON NguoiDung AFTER INSERT, UPDATE
 AS
 BEGIN
 	UPDATE NguoiDung SET MatKhau = HASHBYTES('SHA2_512', I.TenDangNhap + '@!?#?' + I.MatKhau)
-    FROM NguoiDung ND INNER JOIN Inserted I ON I.MaNguoiDung = ND.MaNguoiDung
+	FROM NguoiDung ND INNER JOIN Inserted I ON I.MaNguoiDung = ND.MaNguoiDung
 END
 GO
 
